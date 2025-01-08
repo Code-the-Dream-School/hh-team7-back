2567fb92245142801a7d140e255fff9a94684fd7
Kate <mykateWgmail.com>
2025-01-08 14:34:53 -0800

    change in userRoutes-added admin and user specific routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,17 +1,51 @@
 const express = require('express');
 const { register, login, logout, passwordResetRequest, passwordResetVerify, passwordResetUpdate, getUsers, updateUser, getUserById, deleteUser } = require('../controllers/userController');
 const router = express.Router();
+const roles = require("../config/roles");
+const {
+  authorizeRoles,
+  verifyRoleInDB,
+} = require("../middleware/role-authorization");
+const upload = require('../middleware/multerMiddleware');
 
 // API v1 Routes
+// public routes
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/logout').post(logout);
 router.route('/password-reset/request').post(passwordResetRequest);
 router.route('/password-reset/verify').post(passwordResetVerify);
 router.route('/password-reset/update').post(passwordResetUpdate);
-router.get('/', getUsers);
-router.get('/:id', getUserById);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
+
+// Admin routes
+router.get(
+    '/',
+    authorizeRoles([roles.ADMIN]),
+    verifyRoleInDB([roles.ADMIN]),
+    getUsers
+  ); 
+  
+// user routes - both admins and the users themselves are allowed to update or delete their data.
+router.get(
+    '/:id', 
+    authorizeRoles([roles.ADMIN, roles.USER]),
+    verifyRoleInDB([roles.ADMIN, roles.USER]),    
+    getUserById
+  );
+  
+router.put(
+    '/:id',
+    authorizeRoles([roles.ADMIN, roles.USER]),
+    verifyRoleInDB([roles.ADMIN, roles.USER]),
+    upload,
+    updateUser
+  );
+  
+router.delete(
+    '/:id',
+    authorizeRoles([roles.ADMIN, roles.USER]),
+    verifyRoleInDB([roles.ADMIN, roles.USER]),
+    deleteUser
+  );
 
 module.exports = router;
